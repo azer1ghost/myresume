a380db37e0466cb76cd80269174e75a6e6d90de3
Azer <mamedovazer124@gmail.com>
2020-12-16 15:09:31 +0400

    first

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,17 @@
+<!-- Menu -->
+<div class="circle-menu">
+    <div class="hamburger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+</div>
+<div class="inner-menu js-menu">
+    <ul class="nav">
+        <li class="nav__item"><a href="about.html">About</a></li>
+        <li class="nav__item"><a class="active" href="resume.html">Resume</a></li>
+        <li class="nav__item"><a href="portfolio.html">Portfolio</a></li>
+        <li class="nav__item"><a href="blog.html">Blog</a></li>
+        <li class="nav__item"><a href="contact.html">Contact</a></li>
+    </ul>
+</div>
---
+++ resources/views/components/slidenav.blade.php
@@ -0,0 +1,14 @@
+<!-- Demo Menu -->
+<div class="btnSlideNav slideOpen"></div>
+<div class="btnSlideNav slideClose"></div>
+<ul class="slideNav">
+    <li class="slideNav__item rtl-mode"><h4 class="title title--5">More pages</h4> <a href="rtl/about.html">RTL</a></li>
+    <li class="slideNav__item"><a href="one-page.html">1. Single page</a></li>
+    <li class="slideNav__item"><a href="background-3.html">2. Background peach</a></li>
+    <li class="slideNav__item"><a href="background-2.html">3. Background green</a></li>
+    <li class="slideNav__item"><a href="background-5.html">4. Background green 2</a></li>
+    <li class="slideNav__item"><a href="background-4.html">5. Background triangles</a></li>
+    <li class="slideNav__item"><a href="menu_alternative/about.html">6. Alternative menu</a></li>
+</ul>
+<div class="overlay-slideNav"></div>
+<!-- Demo Menu -->
